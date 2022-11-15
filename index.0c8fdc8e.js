186473a150bcb106858c3c319d82c0dc3a8e4d24
IngaPirogova <105660161+IngaPirogova@users.noreply.github.com>
2022-11-15 11:36:56 +0000

    Deploying to gh-pages from @ IngaPirogova/goit-js-hw-11@ac57a5c2ff454c94e11a353525c6228509b94cb2 🚀

---
+++ index.0c8fdc8e.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("#search-form");console.log(o)}();
+//# sourceMappingURL=index.0c8fdc8e.js.map
---
+++ index.55bafaed.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("#search-form");console.log(o);
+//# sourceMappingURL=index.55bafaed.js.map
