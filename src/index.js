79e1fe26a23b4185511008a4b30294633f17140d
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-11-16 14:31:41 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,4 +1,97 @@
+import axios from "axios"
+import fetchImages from "./partials/js/fetch";
+import { Notify } from "notiflix";
+
 const formSearch = document.querySelector('#search-form')
-console.log(formSearch)
+const gallery = document.querySelector('.gallery')
+
+let page = 1;
+let currentHits = 0;
+let searchQuery = '';
+
+ formSearch.addEventListener('submit', onSearchForm);
+
+ async function onSearchForm(e) {
+    e.preventDefault();    
+    const searchQuery = e.currentTarget.elements.searchQuery.value;   
+    console.log(searchQuery)        
+    if (!searchQuery) {
+      return;
+    } 
+    perPage = 0;
+    currentPage = 1;
+         
+    renderMarkup()
+    appendImgMarkup()    
+}
+
+async function appendImgMarkup() {
+  try {
+    const resp = await fetchImages(searchQuery, page);          
+    const {
+      data: { hits, totalHits },
+    } = response; 
+    gallery.insertAdjacentHTML('beforeend', renderMarkup(hits)); 
+
+    if (hits.length === 0 && totalHits === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );  
+      return;
+    } 
+        
+  } catch (error) {
+    console.log(error);
+  }  
+} 
+
+function renderMarkup(hits) {
+        return hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          likes,
+          views,
+          comments,
+          tags,
+          downloads,
+        }) => {
+          return `<div class='photo-card'>
+      <a href='${largeImageURL}'><img
+          src='${webformatURL}'
+          alt='${tags}'
+          loading='lazy'
+        /></a>
+      <div class='info'>
+        <p class='info-item'>
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class='info-item'>
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class='info-item'>
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class='info-item'>
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+    </div>`;
+        }
+      )
+      .join('');          
+  }
+  
+    
+ 
+
+
+
+
 
 
---
+++ src/partials/js/fetch.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//ф-ция поиска и ретурн результата поиска
+
+export async function fetchImages(searchQuery, page) {
+
+const KEY_API = '31233349-657dbeb08b09bae80b555b3c4';
+const BASE_URL = 'https://pixabay.com/api/';
+const options = `?key=${KEY_API}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    
+const searchImg = await axios.get(`${BASE_URL}${options}`)
+return searchImg.data
+}
+
+
+
+
