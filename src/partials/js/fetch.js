a7205f061c3f260c1891022cd37fb0df8156283c
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-04-01 16:03:18 +0300

    Update fetch.js

--- src/partials/js/fetch.js
+++ src/partials/js/fetch.js
@@ -1,5 +1,7 @@
 const axios = require('axios').default;
+
 export default async function fetchImages(name, page, per_page) {
+  
   try {
     const response = await axios.get(
       `https://pixabay.com/api/?key=31233349-657dbeb08b09bae80b555b3c4&q=${name}&page=${page}&per_page=${per_page}&image_type=photo&orientation=horizontal&safesearch=true`,
